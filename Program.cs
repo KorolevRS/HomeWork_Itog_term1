d4cdad4adc999acf0642495088f6aab53bfed7aa
Roman <romeoone@mail.ru>
2022-12-25 16:35:36 +0300

    Корректировка решения задачи и добавление в репозиторий БлокСхемы

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ string[] array = Console.ReadLine()!.Split(',');
 int j = 0;
 for (int i = 0; i < array.Length; i++) //подсчет количества элементов нового массива
     if (array[i].Length <= 3) j++;
-string[] newArray = new string[j + 1]; // опрелили размерность нового массива
 if (j == 0) //если элементов с длиной менее 3 символов нет в массив, 
             //прерываем программу и далее по коду не идем
 {
-    Console.WriteLine("Элементов с длиной <= 3 символов нет");
-    Environment.Exit(0);
+    Console.WriteLine("В исходном массиве элементов с длиной <= 3 символов нет");
+    Environment.Exit(0); // выход из программы
 }
+
+string[] newArray = new string[j + 1]; // опрелили размерность нового массива
 j = 0;
 for (int i = 0; i < array.Length; i++) //заполняем новый массив элементами, длина которых <= 3 символов
     if (array[i].Length <= 3)
