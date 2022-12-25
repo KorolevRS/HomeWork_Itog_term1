7d277440d4de1710221637f2d5181c59b3d69ac7
Roman <romeoone@mail.ru>
2022-12-25 12:12:03 +0300

    Решение задачи 6-35 строка - с помощью массивов; 38-55 - с применением списка List

--- Program.cs
+++ Program.cs
@@ -2,25 +2,55 @@
  длина которых меньше, либо равна 3 символам. 
  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
 Console.WriteLine($"Введите массив строк через (без пробелов) ','");
 string[] array = Console.ReadLine()!.Split(',');
-string[] newArray = new string[array.Length];
 int j = 0;
-for (int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++) //подсчет количества элементов нового массива
+    if (array[i].Length <= 3) j++;
+string[] newArray = new string[j + 1]; // опрелили размерность нового массива
+if (j == 0) //если элементов с длиной менее 3 символов нет в массив, 
+            //прерываем программу и далее по коду не идем
+{
+    Console.WriteLine("Элементов с длиной <= 3 символов нет");
+    Environment.Exit(0);
+}
+j = 0;
+for (int i = 0; i < array.Length; i++) //заполняем новый массив элементами, длина которых <= 3 символов
     if (array[i].Length <= 3)
     {
         newArray[j] = array[i];
-        // Console.WriteLine($"{newArray[j]}");
         j++;
     }
+Console.Write($"Первоначальный массив: ");
 Print(array);
-Console.WriteLine();
+Console.Write($"Новый массив: ");
 Print(newArray);
 
-void Print(string[] arr)
+void Print(string[] arr)//вывод массива на экран
 {
     for (int i = 0; i < arr.Length; i++)
         Console.Write($"{arr[i]} ");
+    Console.WriteLine();
 }
 
+/* решение задачи с помощью списка List
+using System.Collections.Generic;
+Console.WriteLine($"Введите массив строк через (без пробелов) ','");
+string[] array = Console.ReadLine()!.Split(',');
+List<string> newArray = new List<string>();
+for (int i = 0; i < array.Length; i++)
+   if (array[i].Length <= 3) newArray.Add(array[i]);
+Console.WriteLine();
+Print(array);
 
+for (int i = 0; i < newArray.Count; i++) //вывод массива(списка) типа list
+   Console.Write($"{newArray[i]} ");
+Console.WriteLine();
+void Print(string[] arr)//вывод массива на экран
+{
+   for (int i = 0; i < arr.Length; i++)
+       Console.Write($"{arr[i]} ");
+   Console.WriteLine();
+}
+*/
